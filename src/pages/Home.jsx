877e305ffbaf2d012f1b82057aa4271952187774
Fazal Khan <fazalkhan@Fazals-MacBook-Pro.local>
2026-08-14 14:41:47 +0500

    Add product features (AISalesperson + RuleEngine) to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from '../components/sections/Hero';
 import IntegrationBar from '../components/sections/IntegrationBar';
+import AISalesperson from '../components/sections/AISalesperson';
+import RuleEngine from '../components/sections/RuleEngine';
 
 const Home = () => {
   return (
     <>
       <Hero />
       <IntegrationBar />
+      <AISalesperson />
+      <RuleEngine />
     </>
   );
 };
